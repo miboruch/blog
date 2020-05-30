cc77f31a237a6e18f8337d9b7aeb7d872ac73b7a
Michał Boruch <miboruch@gmail.com>
2020-05-30 12:24:03 +0200

    Changed database url

--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 export const config = {
   port: process.env.PORT || 3000,
+  // databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://miboruch:asdasd123@cluster0-34q83.mongodb.net/test?retryWrites=true&w=majority',
   databaseUrl: process.env.MONGODB_URI || 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
   JwtSecret: 'ajuwhqywekqwe12'
 };
